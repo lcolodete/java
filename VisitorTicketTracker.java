cd93dcb47bc9868c8324a162d69fe16b9af11da1
Leandro Colodete <lcolodete@gmail.com>
2017-03-20 14:11:24 -0300

    Singleton sample

---
+++ VisitorTicketTracker.java
@@ -0,0 +1,25 @@
+
+/**
+ * Singleton thread-safe with Double-Checked Locking
+ * 
+ * @author ur5g
+ *
+ */
+public class VisitorTicketTracker {
+
+	private VisitorTicketTracker() {
+	}
+	
+	private static volatile VisitorTicketTracker instance;
+	
+	public static VisitorTicketTracker getInstance() {
+		if (instance == null) {
+			synchronized(VisitorTicketTracker.class) {
+				if (instance == null) {
+					instance = new VisitorTicketTracker();
+				}
+			}
+		}
+		return instance;
+	}
+}
